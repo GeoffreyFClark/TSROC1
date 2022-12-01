54ee1cda2b1687558a2b38f667e7072be6d9a207
GeoffreyFClark <97141856+GeoffreyFClark@users.noreply.github.com>
2022-12-01 07:13:07 -0700

    a

---
+++ src/nodemirc1.js
@@ -0,0 +1,5 @@
+"use strict";
+var irc = require('irc');
+var client = new irc.Client('bitcoin.uk.eu.dal.net', 'NickTestG777', {
+    channels: ['#Chess'],
+});
---
+++ src/nodemirc1.ts
@@ -0,0 +1,4 @@
+var irc = require('irc');
+var client = new irc.Client('bitcoin.uk.eu.dal.net', 'NickTestG777', {
+    channels: ['#Chess'],
+});
