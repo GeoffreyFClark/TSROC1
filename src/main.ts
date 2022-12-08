c2239b71a310a3a0ffa7489c367ff5a60d28a60a
GeoffreyFClark <geoffreyc240@gmail.com>
2022-12-08 10:50:19 -0700

    update

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+var irc = require('irc');
+var client = new irc.Client('eu.chat4all.org', 'potato', {
+    channels: ['#mircbottest2'],
+    port: 6667,
+});
+
+console.log('Hello World');
+
+client.say('#Chess', "I'm a bot!");
